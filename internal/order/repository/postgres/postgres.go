e6b2633ff216d8db9867d8c4b95c4f88f13451b8
alex.kopcak <alex.kopcak@gmail.com>
2022-05-03 01:09:01 +0300

    comTag.RowsAffected

--- internal/order/repository/postgres/postgres.go
+++ internal/order/repository/postgres/postgres.go
@@ -202,10 +202,12 @@ func (ops *OrderPostgresStorage) Withdrawals(ctx context.Context, userID string)
 
 func (ops *OrderPostgresStorage) UpdateOrder(ctx context.Context, orderNumber string, orderStatus string, orderAccrual int32) error {
 	log.Info().Str("orderStatus", orderStatus).Str("orderNumber", orderNumber).Int32("orderAccurual", orderAccrual).Msg("перед запросом")
-	_, err := ops.db.Exec(ctx,
+	comTag, err := ops.db.Exec(ctx,
 		"UPDATE orders "+
 			"SET order_status = $1 , accrual = $2 "+
 			"WHERE order_id = $3 ;", orderStatus, orderAccrual, orderNumber)
+
+	log.Info().Int64("обновлено", comTag.RowsAffected())
 	log.Info().Err(err)
 	return err
 }
